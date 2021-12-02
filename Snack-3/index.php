9713c137d70e3e6a87d1ee4e4371ec00555ab72d
Laurentiu <lcostin40@gmail.com>
2021-12-02 13:45:56 +0100

    Aggiunto array, codice per stampare in dom l'array. Terminato Snack 3

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -1,4 +1,45 @@
-<?php ?>
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +50,37 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
     
+    foreach ($posts as $data => $post) { 
+
+    ?>
+
+    <p>
+      <?= $data ?> 
+    </p>
+
+    <?php
+
+    for ($i=0; $i < count($post) ; $i++) { 
+        ?>
+
+      <p>
+          <?= $post[$i]["title"] ?>
+      </p>
+
+      <p>
+          <?= $post[$i]["author"] ?>
+      </p>
+
+      <p>
+          <?= $post[$i]["text"] ?>
+      </p>
+      <br>
+
+    <?php  } ?>
+
+<?php  } ?>
+
 </body>
 </html>
