238561c832437689d2f54583763d7d6ee25bf90d
Laurentiu <lcostin40@gmail.com>
2021-12-02 11:38:02 +0100

    Creato array di oggetti

---
+++ Snack_1/index.php
@@ -0,0 +1,41 @@
+<?php 
+
+$decimaGiornataBasket = [ // primo array che contiene le partite
+
+    [
+      "casa" => "Olimpia Milano",
+      "puntiCasa" => 55,
+      "ospite" => "Cantù",
+      "puntiOspite" => 60,
+    ],
+    [
+      "casa" => "Los Angeles Lakers",
+      "puntiCasa" => 70,
+      "ospite" => "Chicago Bulls",
+      "puntiOspite" => 90,
+    ],
+    [
+      "casa" => "San Antonio Spurs",
+      "puntiCasa" => 10,
+      "ospite" => "Golden State Warriors",
+      "puntiOspite" => 105,
+    ]
+  ];
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
