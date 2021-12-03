e26d2880b0b35739eb57afaf21afb8177fe1adb4
Laurentiu <lcostin40@gmail.com>
2021-12-03 12:56:27 +0100

    Creato codice per prendere il nome e stampare se risulta o no tra gli invitati. Terminato Snack 2

--- Blocco_2/Snack_2/index.php
+++ Blocco_2/Snack_2/index.php
@@ -1,7 +1,13 @@
+<?php
+$invitato = $_GET['name'];
+
+?>
+
+
+
 <form action="./invitati.php" method="get">
     <label for="name">nome dell'invitato</label>
     <input type="text" name="name" id="name" >
 </form>
-
 
 
--- Blocco_2/Snack_2/invitati.php
+++ Blocco_2/Snack_2/invitati.php
@@ -1,7 +1,21 @@
 <?php 
-
+$invitato = $_GET['name'];
 $invitati = [
     "Fabio", "Fabiola", "Tiziano"
-]
+];
 
+var_dump($invitati);
+
+if(in_array($invitato, $invitati)){
+    $result = 'ok';
+} else{
+    $result = 'ko';
+}
 ?>
+
+<p>
+    <?= $result ?>
+</p>
+
+
+
